3d9ce41ee0a74ea3f212da0d178fc15bbf5a53b4
St.Dev.Lab <56876553+Stepan-coder@users.noreply.github.com>
2022-05-01 17:18:30 +0300

    new_commit

--- ML develop/Russian Handwriting Recognition/prediction.py
+++ ML develop/Russian Handwriting Recognition/prediction.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 from main import *
 from model import *
 import matplotlib.pyplot as plt
@@ -19,25 +20,31 @@ def predict(model, img):
 model1 = Model3(256, len(ALPHABET))
 model1.load_state_dict(torch.load("checkpoints/checkpoint_epoch_8.pt", map_location=torch.device('cpu')))
 model1.to(DEVICE)
-
-test_dataset = OCRdataset(PATH_TO_TEST_IMGDIR, PATH_TO_TEST_LABELS)
-collator = Collator()
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=2, collate_fn=collator)
-
-examples = []
-idx = 0
 coder = LabelCoder(ALPHABET)
-for batch in test_loader:
-    img, true_label = batch['img'], batch['label']
-    pred_label = predict(model1, img)
-    examples.append([img, true_label, pred_label])
-    idx += 1
-    if idx == 99:
-        break
+collator = Collator()
 
-# fig = plt.figure(figsize=(10, 10))
-rows = int(9 / 4) + 2
-columns = int(9 / 8) + 2
-for j, exp in enumerate(examples):
-    plt.imshow(exp[0][0].permute(2, 1, 0).permute(1, 0, 2))
-    print(j, 'true:' + exp[1][0] + 'pred:' + exp[2][0])
+img = Image.open('aa30.png')
+trans = transforms.Compose([
+    transforms.Grayscale(1),
+    transforms.Resize((64, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
+img = trans(img)
+
+test_loader = torch.utils.data.DataLoader(img, collate_fn=collator)
+for batch in test_loader:
+    pred_label = predict(model1, batch)
+    print(pred_label)
+
+
+
+
+
+
+#
+# # fig = plt.figure(figsize=(10, 10))
+# rows = int(9 / 4) + 2
+# columns = int(9 / 8) + 2
+# for j, exp in enumerate(examples):
+#     plt.imshow(exp[0][0].permute(2, 1, 0).permute(1, 0, 2))
+#     print(j, 'true:' + exp[1][0] + 'pred:' + exp[2][0])
